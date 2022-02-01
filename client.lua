d43925f264fe25809440fde412f30cf2791f5d12
dorq-tr <69513510+dorq-tr@users.noreply.github.com>
2022-02-02 00:39:47 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+local AllowedWeapons = { -- i think only melee weapons
+    GetHashKey("WEAPON_KNIFE"),
+}
+
+Citizen.CreateThread(function()
+    while true do
+        local playerPed = PlayerPedId()
+        for k,v in pairs(AllowedWeapons) do
+            if HasPedBeenDamagedByWeapon(playerPed, v, 0) then
+                local animSet = "move_m@injured"
+                while not HasAnimSetLoaded(animSet) do
+                    RequestAnimSet(animSet)
+                    Citizen.Wait(0)
+                end
+                SetPedMovementClipset(playerPed, animSet, true)
+            end
+        end
+        Citizen.Wait(1)
+    end
+end)
